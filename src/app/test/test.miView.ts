20f1594f351830862a7b1bdb9032a7721df38a23
EDC <d_lalo@hotmail.com>
2019-10-21 16:05:23 -0500

    se agrego directorio de prueba y una vista
    se agrego directorio de prueba y una vista

---
+++ src/app/test/test.miView.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-miView',
+  templateUrl: 'test.page.html',
+  styleUrls: ['test.miView.scss'],
+})
+
+export class toPage {
+
+  constructor() {}
+
+}
